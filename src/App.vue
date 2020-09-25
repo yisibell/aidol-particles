49651b8586d7aa54d6432bf7edf1ac4bf8f46ea6
elenh <yisiwings@163.com>
2020-09-25 15:46:21 +0800

    init.

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <ai-particles />
+  </div>
+</template>
+
+<script>
+import AiParticles from './pkg/main'
+
+export default {
+  name: 'App',
+  components: {
+    AiParticles
+  },
+  data() {
+    return {}
+  },
+  methods: {}
+}
+</script>
---
+++ src/pkg/main.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="ai-particles"> test </div>
+</template>
+
+<script>
+export default {
+  name: 'AiParticles',
+  data() {
+    return {}
+  }
+}
+</script>
